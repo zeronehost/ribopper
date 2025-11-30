43aa00b79b14f3b8edffe002b39349f45c798032
jinbin <1447281971@qq.com>
2025-11-30 22:06:01 +0800

    feat: Add logger module and update editor config

--- crates/app/src/lib.rs
+++ crates/app/src/lib.rs
@@ -1,26 +1,56 @@
+use tauri::Manager;
+
+mod logger;
+
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
-  tauri::Builder::default()
-    .plugin(tauri_plugin_clipboard_manager::init())
+  let ctx = tauri::generate_context!();
+  let mut builder = tauri::Builder::default();
+
+  builder = builder
+    .plugin(tauri_plugin_single_instance::init(|_, _, _| {}))
     .plugin(
       tauri_plugin_log::Builder::new()
-        .level(tauri_plugin_log::log::LevelFilter::Info)
+        .targets(crate::logger::targets())
+        .level(crate::logger::level())
+        .with_colors(tauri_plugin_log::fern::colors::ColoredLevelConfig::default())
+        .max_file_size(crate::logger::MAX_FILE_SIZE)
+        .format(|o, m, r| {
+          o.finish(format_args!(
+            "{}[{}] {}",
+            chrono::Local::now().format("%Y-%m-%d %H:%M:%S"),
+            r.level(),
+            m
+          ))
+        })
         .build(),
     )
     .plugin(tauri_plugin_autostart::Builder::new().build())
+    .plugin(tauri_plugin_clipboard_manager::init())
     .plugin(tauri_plugin_opener::init())
-    .plugin(tauri_plugin_global_shortcut::Builder::new().build())
-    .plugin(tauri_plugin_single_instance::init(|app, args, cwd| {}))
-    .setup(|app| {
-      if cfg!(debug_assertions) {
-        app.handle().plugin(
-          tauri_plugin_log::Builder::default()
-            .level(log::LevelFilter::Info)
-            .build(),
-        )?;
-      }
-      Ok(())
-    })
-    .run(tauri::generate_context!())
+    .plugin(tauri_plugin_global_shortcut::Builder::new().build());
+
+  let app = builder
+    .setup(|app| Ok(()))
+    .build(ctx)
     .expect("error while running tauri application");
+
+  app.run(|app, event| match event {
+    tauri::RunEvent::WindowEvent {
+      label,
+      event: win_e,
+      ..
+    } => match win_e {
+      tauri::WindowEvent::CloseRequested { api, .. } => {
+        let w = app.get_webview_window(label.as_str()).unwrap();
+        w.hide().unwrap();
+        api.prevent_close();
+      }
+      // tauri::WindowEvent::Moved(_pos) => {
+      //   crate::window::set_window_pos(app, label.as_str()).unwrap();
+      // }
+      _ => {}
+    },
+    _ => {}
+  })
 }
---
+++ crates/app/src/logger/mod.rs
@@ -0,0 +1,34 @@
+pub(crate) fn targets() -> Vec<tauri_plugin_log::Target> {
+  if cfg!(debug_assertions) {
+    vec![
+      tauri_plugin_log::Target::new(tauri_plugin_log::TargetKind::Stdout),
+      // tauri_plugin_log::Target::new(tauri_plugin_log::TargetKind::LogDir {
+      //   file_name: Some(format!(
+      //     "ribo-dashboard_dev-{}",
+      //     chrono::Local::now().format("%Y-%m-%d")
+      //   )),
+      // }),
+      tauri_plugin_log::Target::new(tauri_plugin_log::TargetKind::Webview),
+    ]
+  } else {
+    vec![
+      tauri_plugin_log::Target::new(tauri_plugin_log::TargetKind::LogDir {
+        file_name: Some(format!(
+          "ribo-dashboard-{}",
+          chrono::Local::now().format("%Y-%m-%d")
+        )),
+      }),
+      tauri_plugin_log::Target::new(tauri_plugin_log::TargetKind::Webview),
+    ]
+  }
+}
+
+pub(crate) fn level() -> log::LevelFilter {
+  if cfg!(debug_assertions) {
+    log::LevelFilter::Debug
+  } else {
+    log::LevelFilter::Info
+  }
+}
+
+pub(crate) const MAX_FILE_SIZE: u128 = 1024 * 1024 * 10; // 10MB
