ac08ec0f03b9d5f89c7602e241398c41c3f1ecb8
jinbin <1447281971@qq.com>
2025-12-28 18:37:27 +0800

    fix: 修复路径报错

--- crates/app/src/utils/path.rs
+++ crates/app/src/utils/path.rs
@@ -1,7 +1,11 @@
-use std::path::PathBuf;
+use std::{fs::create_dir_all, path::PathBuf};
 
 use tauri::{AppHandle, Manager, Runtime};
 
 pub fn get_ribo_db_path<R: Runtime>(app: &AppHandle<R>) -> anyhow::Result<PathBuf> {
-  Ok(app.path().app_data_dir()?)
+  let p = app.path().app_data_dir()?;
+  if !p.exists() {
+    create_dir_all(&p)?;
+  }
+  Ok(p)
 }
